d84b4bd41a37e33fe423c6b24c50a34e0162b28e
Patrik Bendik <bendikp02@Gmail.com>
2022-02-17 19:56:18 +0100

    2.feladat

--- SorozatKiir/Program.cs
+++ SorozatKiir/Program.cs
@@ -10,6 +10,11 @@ namespace SorozatKiir
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+                Console.ReadKey();
+            }
         }
     }
 }
